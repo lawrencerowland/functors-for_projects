64ece8fa036f8342d3e1f8eae04527b95b5c525c
lawrence rowland <lawrence.g.rowland@me.com>
2025-07-02 14:23:29 -0700

    Revert website app to initial version

--- apps/website/src/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { expect, test } from 'vitest';
-import App from './App';
-
-test('renders heading', () => {
-  render(<App />);
-  const heading = screen.getByText(/^Welcome$/i);
-  expect(heading).toBeDefined();
-});
