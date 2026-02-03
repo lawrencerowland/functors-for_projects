b50f864246ae827ab3dbffa22846710f6159c034
lawrence rowland <lawrence.g.rowland@me.com>
2026-02-03 23:27:40 +0000

    Update app index entry for fibration example

--- apps/website/src/components/PageCard.jsx
+++
@@ -1,7 +0,0 @@
-export default function PageCard({ children }) {
-  return (
-    <div className="rounded-xl shadow-sm bg-white p-6 hover:shadow-md transition">
-      {children}
-    </div>
-  );
-}
